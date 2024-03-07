d0c05ed8ed7f723c31d4d414314ffb5c28552ddd
FayC4l <tazibet61@gmail.com>
2024-03-06 20:12:20 -0500

    secend changes

--- model/login.js
+++ model/login.js
@@ -1,19 +1,23 @@
+// Importing necessary modules
 import { connectionPromise } from '../connexion.js'
 import bcrypt from 'bcrypt';
 
+// Function to hash a password
 function hashpass(password) {
     return new Promise(resolve => {
+        // Generate a salt and hash the password
         bcrypt.genSalt(parseInt(process.env.SALT), function (err, salt) {
             bcrypt.hash(password, salt, function (err, hash) {
                 resolve(hash);
             });
         });
     });
-
 }
 
+// Function to compare a password with a hash
 function comparehash(password, hash) {
     return new Promise(resolve => {
+        // Compare the password with the hash
         bcrypt.compare(password, hash, function (err, result) {
             if (result) {
                 resolve(true);
@@ -25,7 +29,7 @@ function comparehash(password, hash) {
     });
 }
 
-
+// Function to check if an email exists in the database
 export async function emailExiste(email) {
     let connection = await connectionPromise;
     let result = await connection.all(
@@ -36,6 +40,7 @@ export async function emailExiste(email) {
     return true;
 }
 
+// Function to create a new account
 export async function creerUnCompte(nom, prenom, email, password) {
     let connection = await connectionPromise;
     await connection.run(
@@ -45,6 +50,7 @@ export async function creerUnCompte(nom, prenom, email, password) {
     );
 }
 
+// Function to authenticate a user
 export async function connecte(email,password, cb) {
 
     let connection = await connectionPromise;
@@ -63,24 +69,26 @@ export async function connecte(email,password, cb) {
     }
 }
 
+// Function to validate the sign in form
 export function checksingin(body) {
     if (!ValidateEmail(body.email)) {
         return "email pas correct!";
     }
     if (!/^[a-zA-Z ]+$/.test(body.nom)) {
-        return "nom invalide!";
+        return "nom invalide";
     }
     if (!/^[a-zA-Z ]+$/.test(body.prenom)) {
-        return "prenom invalide!";
+        return "prenom invalide";
     }
     if (body.motdepasse.length < 8) {
-        return "mot de passe faible!";
+        return "mot de passe trop court";
     }
     if (body.motdepasse != body.remotdepasse) {
-        return "mot de passe faible!";
+        return "Les mots de passe ne correspends pas";
     }
 }
 
+// Function to validate an email address
 function ValidateEmail(mail) {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
         return (true)
@@ -88,6 +96,7 @@ function ValidateEmail(mail) {
     return (false)
 }
 
+// Function to check if a user is connected
 export function isconnected(request) {
     let user = request.session.passport.user;
     const response = {
--- server.js
+++ server.js
@@ -74,9 +74,10 @@ passport.deserializeUser(async (user, done) => {
         done(error);
     }
 });
-
+// Use body-parser middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Function to check if a user is connected
 function isconn(request) {
     if (request.session.passport)
         if (request.session.passport.user) {
@@ -86,6 +87,7 @@ function isconn(request) {
     return false
 }
 
+// Middleware to initialize passport session if it doesn't exist
 app.use(function (request, response, next) {
     if (request.session.passport == undefined || request.session.passport.user == undefined || request.session.passport.user == null) {
         request.session.passport = {};
@@ -94,8 +96,7 @@ app.use(function (request, response, next) {
     next();
 })
 
-
-
+// Route for the home page
 app.get('/', async (request, response) => {
     response.render('index', {
         titre: 'Menu',
@@ -106,6 +107,7 @@ app.get('/', async (request, response) => {
     });
 });
 
+// Route for the login page
 app.get('/login', async (request, response) => {
     response.render('login', {
         titre: 'login',
@@ -115,8 +117,7 @@ app.get('/login', async (request, response) => {
     });
 });
 
-
-
+// Route for signing in
 app.post('/singin', async (request, response) => {
     const validation = checksingin(request.body);
     if (validation) {
@@ -150,17 +151,17 @@ app.post('/singin', async (request, response) => {
         });
         return;
     }
-    response.redirect("/");
+    
 });
 
-
+// Route for logging in
 app.post('/login', async function (request, response, next) {
     if (!(await emailExiste(request.body.username))) {
         response.render('login', {
             titre: 'login',
             styles: ['/css/style.css'],
             scripts: ['/js/index.js'],
-            message: "email ou mot de passe incorrect!",
+            message: "email ou mot de passe incorrect",
             isconnected: isconnected(request)
         });
         return;
@@ -190,22 +191,25 @@ app.post('/login', async function (request, response, next) {
     })(request, response, next);
 });
 
-
+// Route for logging out
 app.get('/logout', async (request, response, next) => {
+    // Logout the user and redirect to home page
     request.logout(function (err) {
         if (err) { return next(err); }
         response.redirect('/');
     });
 });
 
-
-
+// Route for the shopping cart page
 app.get('/panier', async (request, response) => {
+    // If the user is not connected, redirect to login page
     if (!isconn(request)) {
         response.redirect("/login");
         return;
     }
+    // Get the shopping cart for the connected user
     const panier = request.session.passport.user == {} ? [] : (await getpanier(request.session.passport.user.id_utilisateur));
+    // If the shopping cart is empty, render the empty cart page
     if (panier.length == 0) {
         response.render('panier', {
             titre: 'panier',
@@ -216,11 +220,13 @@ app.get('/panier', async (request, response) => {
         });
         return;
     }
+    // Calculate the total price of the cart
     let total = 0;
     panier.map((element) => {
         total += element.total;
         element.edit = true;
     })
+    // Render the shopping cart page with the cart items and total price
     response.render('panier', {
         titre: 'panier',
         styles: ['/css/style.css'],
@@ -232,27 +238,34 @@ app.get('/panier', async (request, response) => {
     });
 });
 
+// Function to round a number to a certain number of decimals
 function round(value, decimals) {
     return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals).toFixed(decimals);
 }
 
+// Route for the shopping cart page with a specific id
 app.get('/pan', async (request, response) => {
+    // If the user is not connected, redirect to login page
     if (!isconn(request)) {
         response.redirect("/login");
         return;
     }
+    // Get the shopping cart for the connected user with a specific id
     let panier = await getpanierwithid(request.session.passport.user.id_utilisateur, request.query.id);
 
+    // If the user is an admin, get the shopping cart for the admin
     if (request.session.passport.user.id_type_utilisateur && request.session.passport.user.id_type_utilisateur == 2) {
         panier = await getpanierforadmin(request.query.id);
     } else {
         panier = await getpanierwithid(request.session.passport.user.id_utilisateur, request.query.id);
     }
+    // Calculate the total price of the cart
     let total = 0;
     panier.map((element) => {
         total += element.total;
         element.edit = false;
     })
+    // Render the shopping cart page with the cart items and total price
     response.render('panier', {
         titre: 'panier',
         styles: ['/css/style.css'],
@@ -263,13 +276,17 @@ app.get('/pan', async (request, response) => {
     });
 });
 
+// Route for the order page
 app.get('/commande', async (request, response) => {
+    // If the user is not connected, redirect to login page
     if (!isconn(request)) {
         response.redirect("/login");
         return;
     }
+    // If the user is an admin, get all the orders for the admin
     if (request.session.passport.user.id_type_utilisateur == 2) {
         const commands = await getallcommand(request.session.passport.user.id_utilisateur);
+        // Render the order page for the admin
         response.render('commande', {
             titre: 'panier',
             styles: ['/css/style.css'],
@@ -278,8 +295,10 @@ app.get('/commande', async (request, response) => {
             isconnected: isconnected(request)
         });
     }
+    // If the user is not an admin, get all the orders for the user
     else {
         const commands = await getallcommandById(request.session.passport.user.id_utilisateur);
+        // Render the order page for the user
         response.render('commandeuser', {
             titre: 'panier',
             styles: ['/css/style.css'],
@@ -289,77 +308,102 @@ app.get('/commande', async (request, response) => {
         });
     }
 });
-
+// Route for updating an order
 app.post('/commande', async (request, response) => {
+    // Check if the user is logged in
     if (request.session.passport.user == {}) {
         response.status(404).send("page 404");
         return;
     }
+    // Check if the user is an admin
     if (request.session.passport.user.id_type_utilisateur != 2) {
         response.status(403).send("vous devait etre un admin pour modifier l'etat");
         return;
     }
+    // Check if the order status is valid
     if (request.body.etat < 2 || request.body.etat > 4) {
         response.status(403).send("etat invalid");
         return;
     }
+    // Update the order status
     const commands = await updatecommande(request.body.id_commande, request.body.etat);
     response.sendStatus(200);
 });
 
+// Route for adding a product to the cart
 app.post('/produit', async (request, response) => {
+    // Check if the user is logged in
     if (!request.session.passport.user.id_utilisateur) {
         response.status(403).send("vous devait vous connecte!");
         return;
     }
+    // Get the current order for the user
     let commade = await getCommande(request.session.passport.user.id_utilisateur);
+    // If there is no current order, create a new one
     if (commade.length == 0) {
         await create_commande(request.session.passport.user.id_utilisateur);
     }
     commade = await getCommande(request.session.passport.user.id_utilisateur);
+    // Add the product to the order
     await addproduit(commade[0].id_commande, +request.body.id_produit, request.body.quantite, true);
     response.sendStatus(201);
 });
 
+// Route for updating a product in the cart
 app.post('/updateproduit', async (request, response) => {
+    // Check if the user is logged in
     if (request.session.passport.user == {}) {
         response.sendStatus(403);
         return;
     }
+    // Get the current order for the user
     let commade = await getCommande(request.session.passport.user.id_utilisateur);
+    // If there is no current order, return an error
     if (commade.length == 0) {
         response.status(404).send("commande non trouve");
     }
+    // Update the product in the order
     await addproduit(commade[0].id_commande, +request.body.id_produit, request.body.quantite);
     response.sendStatus(200);
 });
 
+// Route for sending an order
 app.post('/sendcommande', async (request, response) => {
+    // Check if the user is logged in
     if (request.session.passport.user == {}) {
         response.sendStatus(403);
         return;
     }
+    // Send the order
     await passelacommande(request.session.passport.user.id_utilisateur);
     response.sendStatus(200);
 });
+
+// Route for deleting all items in the cart
 app.post('/deletepanier', async (request, response) => {
+    // Check if the user is logged in
     if (request.session.passport.user == {}) {
         response.status(403);
         return;
     }
+    // Delete all items in the cart
     await deleteallCommande(request.session.passport.user.id_utilisateur);
     response.sendStatus(200);
 });
 
-
+// Load SSL credentials
 let credentials = {
     key: fs.readFileSync('./https/key.key'),
     cert: fs.readFileSync('./https/cert.cert')
 };
-// Démarrer le serveur
+
+// Create an HTTPS server with the SSL credentials
 let httpsServer = https.createServer(credentials, app);
+
+// Start the server
 if(process.env.NODE_ENV === 'production')
-httpsServer.listen(process.env.PORT);
+    httpsServer.listen(process.env.PORT); // Use HTTPS in production
 else
-app.listen(process.env.PORT);
+    app.listen(process.env.PORT); // Use HTTP in development
+
 console.log(`Serveur démarré sur: http${(process.env.NODE_ENV === 'production')?"s":''}://localhost:${process.env.PORT}`);
